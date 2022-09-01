541eafd4d930b18a59575918ff6674a350f57641
mmantle-hud <m.e.mantle@hud.ac.uk>
2022-09-01 08:43:16 +0100

    Update form-process.php

--- form-process.php
+++ form-process.php
@@ -2,22 +2,15 @@
 $email = $_POST["email"];
 
 /*
-Add code that will also display the values from the other text fields and the radio buttons. Make sure you display the values from each of the different form controls in the body of the HTML page. You will also need to make sure you have selected one of the radio buttons or you will get errors.
+This page contains code that will display the email address the user entered.
+Add code that will also display the values from the other text fields and the radio buttons. 
+Make sure you display the values from each of the different form controls in the body of the HTML page. 
+You will also need to make sure you have selected one of the radio buttons or you will get errors.
 */
 
 /*
-Displaying the value of the checkbox isn't very useful. Instead we want to know if the user has clicked the checkbox and agreed to the terms and conditions. Uncomment the following code that tests the checkbox.
-*/
-
-/*
-$terms = false;
-if(isset($_POST["terms"])){
-  $terms = true;
-}
-*/
-
-/*
-In the body of the HTML page add an if statement that tests if the variable $terms is true. If $terms is true the user should receive a message stating 'You agreed to the terms and conditions.' if $terms is false they should receive a message stating 'You didn't agree to the terms and conditions.'
+In the html-forms.html page add another form control to gather the user's phone number. 
+Display the entered phone number below along with the other values from the form. 
 */
 
 /*
